d024bd198ecde861021113e7869e94234699154f
Ankelo-nqn <angel_cellillo@hotmail.com>
2022-12-11 19:07:16 -0300

    modificamos intro.md en full-stack

--- docs/full-stack/intro.md
+++ docs/full-stack/intro.md
@@ -3,3 +3,8 @@
 Curso Full Stack
 
 Telefónica
+
+
+Curso básico de PHP, MySQL, Bootstrap, Java, HTML, CSS
+
+Mucho no?
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,30 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: 'Sitio de El Ankelo',
+  tagline: 'El Ankelo is very cool!',
+  
+
+  // url: 'https://your-docusaurus-test-site.com',
+  // baseUrl: '/',
+
+  url: 'https://Ankelo-nqn.github.io',
+  baseUrl: '/docusaurus	',
+  organizationName: 'Ankelo-nqn',
+  projectName: 'docusaurus',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: 'false',
+
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+
+  // organizationName: 'facebook', // Usually your GitHub org/user name.
+  // projectName: 'docusaurus', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
